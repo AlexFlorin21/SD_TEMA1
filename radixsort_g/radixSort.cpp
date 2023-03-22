d0cc81b56c415bee593aa55f441b44a3f7a0d5b5
AlexFlorin21 <119683630+AlexFlorin21@users.noreply.github.com>
2023-03-22 19:48:08 +0200

    Update radixSort.cpp

--- radixsort_g/radixSort.cpp
+++ radixsort_g/radixSort.cpp
@@ -8,18 +8,20 @@ int i,p=1,c,max,cnt=0;
 for(i=0;i<n-1;i++)
         if(v[i]>v[i+1]) max=v[i];
             else max=v[i+1];
+  ///calculam baza maxima 
 while(max!=0){
     cnt=cnt+1;
     max=max/10;
             }
+        //cat timp avem nr in baza
 while(cnt!=0){
     int b[n]={0},a[10]={0};
     for(i=0;i<n;i++){
         c=(v[i]/p)%10;
         a[c]++;     }
-    for(i=1;i<10;i++) a[i]=a[i]+a[i-1];
-    for(i=n-1;i>=0;i--){
-        b[a[(v[i]/p)%10]-1]=v[i];
+    for(i=1;i<10;i++) a[i]=a[i]+a[i-1];  ///stabilim numarul de numere
+    for(i=n-1;i>=0;i--){            ////Pentru fiecare element din v, această secțiune de cod ia cifra corespunzătoare poziției actuale 
+        b[a[(v[i]/p)%10]-1]=v[i];   ////și o utiliză ca un indice în a. Valoarea din a la această poziție reprezintă poziția corespunzătoare în b.
         a[(v[i]/p)%10]--;}
     for (i=0;i<n;i++) v[i]=b[i];
     cnt--;
